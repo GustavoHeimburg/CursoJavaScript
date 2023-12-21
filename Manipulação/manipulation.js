5a5362b24bb654db761d0147c6f89ac0edbc4e2c
GustavoHeimburg <gutogustavogu@gmail.com>
2023-12-21 10:16:04 -0300

    Lógica

--- Manipulação/manipulation.js
+++ Manipulação/manipulation.js
@@ -1,8 +1,28 @@
-var nome = prompt('digite seu nome:')
-alert(`Seja bem vindo ${nome}`)
+// var nome = prompt('digite seu nome:')
+// alert(`Seja bem vindo ${nome}`)
 
-var pessoa = prompt('Quem voce é?')
+var pessoa = prompt('Como é seu nome?')
+var resposta = prompt('Escolha uma opção:\n1. Pessoa humana\n2. Alienígena\n3. Pessoa jurídica\n4. Animal de estimação (respondendo pelo dono)');
 
-if (1==true){
+var opcao = parseInt(resposta);
 
+switch (opcao){
+    case 1:
+        alert(`${pessoa} Voce e uma pessoa humana.`);
+        break;
+
+    case 2:
+        alert(`${pessoa} Voce e uma alienigena.`);
+        break;
+
+    case 3:
+        alert(`${pessoa} Voce e uma pessoa juridica`);
+        break;
+
+    case 4:
+        alert(`${pessoa}, você é um animal de estimação (respondendo pelo dono).`);
+        break;
+
+    default:
+            alert(`${pessoa} Opcao invalida...`);
 }
